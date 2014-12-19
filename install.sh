9909e417c24b5d0173d0b9172762953cf562fa57
Winnerhust <910894904@qq.com>
2014-12-19 14:41:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #Ubunbut Install Shell
 #Load install shell script
 #本段可能需要手动执行
-wget https://github.com/Winnerhust/Ubuntu-Install/blob/master/install.sh
-grep td install.sh |sed -e 's/<[^>]*>//g' >real_install.sh
-move real_install.sh install.sh
+#wget https://github.com/Winnerhust/Ubuntu-Install/blob/master/install.sh
+#grep td install.sh |sed -e 's/<[^>]*>//g' >real_install.sh
+#move real_install.sh install.sh
 
 # change sources
 #本段可能需要手动执行
